3adb23dde03922d98210a5061493ced429b02622
jonny7kommaklar <jionnsjei@gmail.com>
2026-03-24 18:40:25 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -2,10 +2,10 @@ window.SpotmapConfig = {
   supabaseUrl: 'https://crfxwgkidhljyrwvubnq.supabase.co',
   supabaseAnonKey: 'sb_publishable_CcZD8kOYDjjyQPDM9xc7gA_eTNrMIWR',
   siteTitle: 'Spotmap',
-  defaultCityCover: 'https://images.unsplash.com/photo-1629044562928-6946d8d3feea?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  defaultCityCover: 'https://images.unsplash.com/photo-1518391846015-55a9cc003b25?auto=format&fit=crop&w=1400&q=80',
   cityCovers: {
-    prag: 'https://skatejawn.com/wp-content/uploads/2020/01/11-Paul-Rodriguez-ss-heel-5-0-1140x760.jpg',
-    stuttgart: 'https://images.unsplash.com/photo-1541417904950-b855846fe074?auto=format&fit=crop&w=1400&q=80',
+    prag: 'https://images.unsplash.com/photo-1629044562928-6946d8d3feea?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    stuttgart: 'https://www.staatsanzeiger.de/wp-content/uploads/2025/04/stuttgart-baden-wurttemberg-germany-january-23-2025-the-state-parliament-of-baden-wurttemberg-in-stuttgart-2-768x512.jpeg',
     innsbruck: 'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?auto=format&fit=crop&w=1400&q=80'
   }
 };
