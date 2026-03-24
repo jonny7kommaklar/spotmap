1b6cd98d897ff131c5ed092d035eb2fe1cd9d098
jonny7kommaklar <jionnsjei@gmail.com>
2026-03-24 18:10:52 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -4,7 +4,7 @@ window.SpotmapConfig = {
   siteTitle: 'Spotmap',
   defaultCityCover: 'https://images.unsplash.com/photo-1518391846015-55a9cc003b25?auto=format&fit=crop&w=1400&q=80',
   cityCovers: {
-    prag: 'https://images.unsplash.com/photo-1519677100203-a0e668c92439?auto=format&fit=crop&w=1400&q=80',
+    prag: 'https://skatejawn.com/wp-content/uploads/2020/01/11-Paul-Rodriguez-ss-heel-5-0-1140x760.jpg',
     stuttgart: 'https://images.unsplash.com/photo-1541417904950-b855846fe074?auto=format&fit=crop&w=1400&q=80',
     innsbruck: 'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?auto=format&fit=crop&w=1400&q=80'
   }
